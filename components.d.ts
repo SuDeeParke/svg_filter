c9807146416f3ad6e660f36e355180ef51255fbd
sudongpeng <suxiaopo137@foxmail.com>
2023-07-20 16:24:28 +0800

    feat: 支持图片上传

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,10 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
     ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElImage: typeof import('element-plus/es')['ElImage']
     ElSlider: typeof import('element-plus/es')['ElSlider']
+    ElUpload: typeof import('element-plus/es')['ElUpload']
     FeColorMatrixDemo: typeof import('./src/components/feColorMatrixDemo.vue')['default']
     SvgFilter: typeof import('./src/components/SvgFilter.vue')['default']
   }
--- src/components/SvgFilter.vue
+++ src/components/SvgFilter.vue
@@ -9,17 +9,36 @@
           :values="(matrixResult as unknown as string)" />
 
         <feColorMatrix id="saturate" type="saturate" :values="`${(saturate)}`" />
-        <feColorMatrix type="hueRotate" :values="`${(hueRotate)}`"/>
+        <feColorMatrix type="hueRotate" :values="`${(hueRotate)}`" />
       </filter>
     </defs>
   </svg>
   <main class="container">
     <h1>SvgFilter</h1>
     <section class="display">
-      <img :src="demoPic" alt="">
+      <div class="img-wrap">
+        <img :src="imageUrl" alt="">
+       
+      </div>
+       <div class="upload" >
+            <el-upload
+              drag
+              ref="upload"
+              action=""
+              :show-file-list="false"  
+              :limit="1"
+              :on-exceed="handleExceed"
+              :auto-upload="false"
+              :on-change="handleChange"
+              >
+              <div class="el-upload__text">
+                Drop file here or <em>click to upload</em>
+              </div>
+            </el-upload>
+          </div>
     </section>
     <section class="controler">
-      <el-collapse v-model="activeNames" >
+      <el-collapse v-model="activeNames">
         <el-collapse-item title="颜色矩阵" name="1">
 
           <div class="group" v-for="(array) in matrix">
@@ -33,33 +52,34 @@
               </div>
             </div>
           </div>
-           <div class="btn">
-             <el-button plain @click="reset()">重设</el-button>
-            </div>
+          <div class="btn">
+            <el-button plain @click="reset()" :size="'small'">重设</el-button>
+          </div>
         </el-collapse-item>
-        
+
         <el-collapse-item title="高级项" name="2">
           <div class="slide-item">
             <label>饱和度</label>
             <el-slider v-model="saturate" show-input size="small" :step="0.01" :min="0" :max="1" />
           </div>
           <div class="slide-item">
-              <label>色相</label>
-              <el-slider v-model="hueRotate" show-input size="small" :min="0" :max="360" />
+            <label>色相</label>
+            <el-slider v-model="hueRotate" show-input size="small" :min="0" :max="360" />
           </div>
           <div class="btn">
-            <el-button plain @click="resetAdvance()">重设</el-button>
+            <el-button plain @click="resetAdvance()" :size="'small'">重设</el-button>
           </div>
 
         </el-collapse-item>
       </el-collapse>
 
     </section>
+    <h5>结果</h5>
     <section class="output">
-      <div> 颜色矩阵结果：{{ matrixResult }}</div>
+      <div> 颜色矩阵：{{ matrixResult }}</div>
       <div> 饱和度： {{ saturate }}</div>
       <div> 色相： {{ hueRotate }}</div>
-   
+
     </section>
   </main>
 </template>
@@ -67,7 +87,8 @@
 <script lang="ts" setup>
 import demoPic from '@/assets/images/demo.png';
 import { ref, computed } from 'vue';
-import {cloneDeep } from 'lodash';
+import { cloneDeep } from 'lodash';
+import { genFileId } from 'element-plus'
 
 // 颜色矩阵
 /**
@@ -77,23 +98,18 @@ import {cloneDeep } from 'lodash';
  *  g   0 0 0 0 0
  *  b   0 0 0 0 0
  *  a   0 0 0 0 0
- */ 
+ */
 const defVal = [
   [{ id: 'RR', name: 'Red to red', value: 1 }, { id: 'GR', name: 'Green to red', value: 0 }, { id: 'BR', name: 'Blue to red', value: 0 }, { id: 'AR', name: 'Alpha to red', value: 0 }, { id: 'OR', name: 'red offset', value: 0 }],
   [{ id: 'RG', name: 'Red to green', value: 0 }, { id: 'GG', name: 'Green to green', value: 1 }, { id: 'BG', name: 'Blue to green', value: 0 }, { id: 'AG', name: 'Alpha to green', value: 0 }, { id: 'OG', name: 'green offset', value: 0 }],
   [{ id: 'RB', name: 'Red to blue', value: 0 }, { id: 'GB', name: 'Green to blue', value: 0 }, { id: 'BB', name: 'Blue to blue', value: 1 }, { id: 'AB', name: 'Alpha to blue', value: 0 }, { id: 'OB', name: 'blue offset', value: 0 }],
   [{ id: 'RA', name: 'Red to alpha', value: 0 }, { id: 'GA', name: 'Green to alpha', value: 0 }, { id: 'BA', name: 'Blue to alpha', value: 0 }, { id: 'AA', name: 'Alpha to alpha', value: 1 }, { id: 'OA', name: 'alpha offset', value: 0 }],
 ]
-
 const matrix = ref(cloneDeep(defVal));
-
-
 // 饱和度
 const saturate = ref<number>(1);
-
 // hueRotate
 const hueRotate = ref<number>(0);
-
 const matrixResult = computed(() => {
   return `
     ${matrix.value[0][0].value} ${matrix.value[0][1].value} ${matrix.value[0][2].value} ${matrix.value[0][3].value} ${matrix.value[0][4].value}
@@ -102,8 +118,6 @@ const matrixResult = computed(() => {
     ${matrix.value[3][0].value} ${matrix.value[3][1].value} ${matrix.value[3][2].value} ${matrix.value[3][3].value} ${matrix.value[3][4].value}
   `;
 })
-
-
 const activeNames = ref(['1'])
 
 const reset = () => {
@@ -114,6 +128,26 @@ const resetAdvance = () => {
   saturate.value = 1;
   hueRotate.value = 0;
 }
+
+const upload = ref()
+const handleExceed = (files: any) => {
+  upload.value!.clearFiles()
+  const file = files[0]
+  file.uid = genFileId()
+  upload.value!.handleStart(file)
+}
+
+const imageUrl = ref(demoPic)
+const handleChange= (file:any, fileList:any) => {//选中文件触发的change事件
+  const isJPG = file.raw.type === 'image/jpeg'
+  const isPNG = file.raw.type === 'image/png'
+  if (!isPNG && !isJPG) {
+    return false
+  } else {
+    imageUrl.value = URL.createObjectURL(file.raw);//赋值图片的url，用于图片回显功能
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -126,27 +160,42 @@ const resetAdvance = () => {
 
   .display {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     justify-content: start;
     margin-bottom: 30px;
-    img {
-      width: 20%;
-      filter: url('#f1');
+    width: 100%;
+    .img-wrap{
+      position: relative;
+      img {
+        width: 400px;
+        filter: url('#f1');
+      }
     }
+    .upload{
+      display: block;
+      align-items: center;
+      justify-content: center;
+      margin-left: 20px;
+    }
+    
   }
 
   .controler {
     margin-bottom: 20px;
+
     .group {
       width: 60%;
       display: flex;
       justify-content: space-between;
       margin: 20px 0;
+
       .item {
         display: flex;
+
         label {
           margin-right: 10px;
         }
+
         .result {
           position: relative;
           width: 80px;
@@ -156,6 +205,7 @@ const resetAdvance = () => {
           margin-right: 20px;
 
           cursor: pointer;
+
           .pop {
             display: none;
             position: absolute;
@@ -167,18 +217,19 @@ const resetAdvance = () => {
             z-index: 10;
             padding-top: 20px;
           }
-          &:hover{
+
+          &:hover {
             .pop {
               display: flex;
             }
           }
         }
-        
-      
+
+
       }
     }
 
-    .slide-item{
+    .slide-item {
       max-width: 500px;
       min-width: 300px;
       padding: 0 20px;
@@ -186,7 +237,7 @@ const resetAdvance = () => {
 
   }
 
-  .output{
+  .output {
     width: 100%;
     height: 100%;
     background-color: #333;
@@ -194,12 +245,12 @@ const resetAdvance = () => {
     padding: 20px;
     box-sizing: border-box;
     border-radius: 5px;
+    margin-top: 21px;
   }
 
 }
 
-.btn{
+.btn {
   margin: 20px 0;
 }
-
 </style>
